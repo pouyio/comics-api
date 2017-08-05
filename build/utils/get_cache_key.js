fed7a6b040869de7c3888587a8067bb8e98ee8a0
Vicente <pouyio@gmail.com>
2017-08-05 18:30:27 +0200

    removed babel compilation

--- build/utils/get_cache_key.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var pathToRegexp = require('path-to-regexp');
-
-module.exports = function (template, data) {
-  return pathToRegexp.compile(template)(data);
-};
--- build/utils/setCookie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var sourceServer = require('../source');
-var CONST = require('../constants');
-
-module.exports = async function (req, res, next) {
-  await sourceServer.makeRequest({ url: CONST.SOURCE_URL });
-  next();
-};
