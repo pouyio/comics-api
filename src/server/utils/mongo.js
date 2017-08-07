1d05ccafa9d60838c93904721e8320ff0008f395
Vicente <pouyio@gmail.com>
2017-08-07 18:53:05 +0200

    added reconnection with db if lost

--- src/server/utils/mongo.js
+++ src/server/utils/mongo.js
@@ -5,13 +5,24 @@ const state = {
   db: null,
 }
 
+const getDb = async () => {
+  if(state.db) return state.db;
+  try {
+    const db = await MongoClient.connect(CONST.MONGO_URL);
+    state.db = db;
+    return db;
+  }catch (e) {
+    throw new Error(e);
+  }
+}
+
 const checkCache = async (cacheKey) => {
-  const document = await state.db.collection('cache').findOne({cacheKey}, {_id: 0, result: 1 });
+  const document = await (await getDb()).collection('cache').findOne({cacheKey}, {_id: 0, result: 1 });
   return document ? document.result: '';
 }
 
 const saveCache = async (data, cacheKey) => {
-  await state.db.collection('cache').insert({
+  await (await getDb()).collection('cache').insert({
       "createdAt": new Date(),
       "cacheKey": cacheKey,
       "result": data
@@ -19,15 +30,15 @@ const saveCache = async (data, cacheKey) => {
 }
 
 const retrieveUser = async (user) => {
-  return await state.db.collection('users').findOne({user}, {_id: 0, user: 1 });
+  return await (await getDb()).collection('users').findOne({user}, {_id: 0, user: 1 });
 }
 
 const retrieveComicsRead = async (user) => {
-  return await state.db.collection('read').find({$or: [{ 'issues.0': {$exists: true}}, {wish: true}], user}, {'_id': 0, user: 0}).toArray();
+  return await (await getDb()).collection('read').find({$or: [{ 'issues.0': {$exists: true}}, {wish: true}], user}, {'_id': 0, user: 0}).toArray();
 }
 
 const retrieveIssuesRead = async (comic, user) => {
-  const results = await state.db.collection('read').findOne({comic, user}, {'_id': 0, issues: 1, wish: 1});
+  const results = await (await getDb()).collection('read').findOne({comic, user}, {'_id': 0, issues: 1, wish: 1});
   const issuesRead = (results && results.issues) ? results.issues : [];
   const wish = results? results.wish: false;
   return [issuesRead, wish];
@@ -36,11 +47,11 @@ const retrieveIssuesRead = async (comic, user) => {
 const markIssueRead = async (comic, issue, value, user) => {
   let operation = {};
   operation[value ? '$addToSet': '$pull'] = {issues: issue};
-  return await state.db.collection('read').update({comic, user}, operation, {upsert: true});
+  return await (await getDb()).collection('read').update({comic, user}, operation, {upsert: true});
 }
 
 const markComicWish = async (comic, value, user) => {
-  return await state.db.collection('read').update({comic, user}, {$set: {wish: value}}, {upsert: true});
+  return await (await getDb()).collection('read').update({comic, user}, {$set: {wish: value}}, {upsert: true});
 }
 
 module.exports = {
