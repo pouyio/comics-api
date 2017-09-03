2b1edf4b654d3a1a3559a0bb26c5d6861230dd72
Vicente <pouyio@gmail.com>
2017-09-03 18:36:38 +0200

    fixed but on not wished comics

--- src/server/user-info.js
+++ src/server/user-info.js
@@ -3,8 +3,16 @@ const mongo = require('./utils/mongo');
 const CONST = require('./constants');
 
 router.get(CONST.ROUTES.comic.detail, async (req, res) => {
-  const info = await mongo.retrieveUserComicInfo(req.params.name, req.user);
-  Object.assign(res.locals, {info});
+  const info = await mongo.retrieveUserComicInfo(req.params.name, req.user) || {};
+  for(const i in res.locals.included) {
+    if(res.locals.included[i].type === 'issues') {
+      const issueInfo = res.locals.included[i];
+      const userInfo = info[issueInfo.id] || {read: false, page: 0};
+      res.locals.included[i].read = userInfo.read;
+      res.locals.included[i].page = userInfo.page;
+    }
+  }
+  res.locals.wish = Object.keys(info).length ? info.wish: false;
   res.send(res.locals);
 });
 
