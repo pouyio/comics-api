5261e715edee04bc3277cee5d529ecd92791cc4d
Vicente <pouyio@gmail.com>
2017-10-24 20:39:38 +0200

    removed challenges files

--- src/utils/setCookie.js
+++
@@ -1,7 +0,0 @@
-const sourceServer = require('./source');
-const CONST = require('../constants');
-
-module.exports = async (req, res, next) => {
-  await sourceServer.makeRequest({url: CONST.SOURCE_URL});
-  next();
-}
